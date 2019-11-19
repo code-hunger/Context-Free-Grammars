68b778b63baedd1d31743829b281b0d38eed5568
Code Hunger <beinghavingbreackfast@gmail.com>
2019-11-19 23:11:36 +0200

    Implement proper validation of the grammar state in [CF]GrammarTouple

--- Grammar.h
+++ Grammar.h
@@ -27,19 +27,74 @@ struct Grammar
 	virtual ~Grammar() = default;
 };
 
-template <typename C> class Rule
+template <typename C> struct Rule
 {
 	const C* const from;
 
 	const AlphaString<C> to;
+
+	Rule(C const& from, AlphaString<C>&& to) : from(&from), to(std::move(to)) {}
 };
+
+template <typename C>
+bool pairwiseDistinct(Alphabet<C> const& A, Alphabet<C> const& B)
+{
+	return std::all_of(begin(A), end(A),
+	                   [&B](const C* c) { return !B.findChar(c); });
+}
 
 template <typename C> class GrammarTouple
 {
-	Alphabet<C> N, T;
+	using SharedAlphabet = shared_ptr<Alphabet<C>>;
+
+protected:
+	const SharedAlphabet N, T;
+
+	const Alphabet<C> N_union_T = N + T;
 
 	C start;
 	std::vector<Rule<C>> rules;
+
+public:
+	GrammarTouple(decltype(N) N, decltype(T) T, C start, decltype(rules) rules)
+	    : N(N), T(T), start(start), rules(rules)
+	{
+		if (!N->findChar(start)) {
+			std::ostringstream error;
+			error << "Start character '" << start
+			      << "' not present in the Non-terminals alphabet.";
+			throw std::invalid_argument(error.str());
+		}
+
+		if (!pairwiseDistinct(*N, *T)) {
+			throw std::invalid_argument("The Terminals and the Non-terminals "
+			                            "alphabets must be pairwise distinct.");
+		}
+	};
+};
+
+template <typename C> struct CFGrammarTouple : public GrammarTouple<C>
+{
+	CFGrammarTouple(decltype(GrammarTouple<C>::N) N,
+	                decltype(GrammarTouple<C>::T) T, C start,
+	                decltype(GrammarTouple<C>::rules) rules)
+	    : GrammarTouple<C>(N, T, start, rules)
+	{
+		if (!std::all_of(begin(rules), end(rules), [&N](auto const& rule) {
+			    return N.findChar(rule.from);
+		    })) {
+			throw std::invalid_argument(
+			    "All rules must satisfy N.findChar(rule.from) != nullptr");
+		}
+
+		if (!std::all_of(begin(rules), end(rules),
+		                 [& N_union_T = this->N_union_T](auto const& rule) {
+			                 return rule.to.alphabet.subsetOf(N_union_T);
+		                 })) {
+			throw std::invalid_argument("The 'to' part of all rules must be a "
+			                            "string from the alphabet union N+T.");
+		}
+	}
 };
 
 } // namespace context_free
