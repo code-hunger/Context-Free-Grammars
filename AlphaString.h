2c4fdd196adfa37220e1c539d0aef42eb8dd720b
Code Hunger <beinghavingbreackfast@gmail.com>
2019-11-20 23:40:35 +0200

    An AlphaString needs to store only an AlphabetLike, not a full Alphabet
    This will allow us to use an AlphabetTouple for an AlphaString's
    alphabet.

--- AlphaString.h
+++ AlphaString.h
@@ -7,7 +7,7 @@ using std::shared_ptr;
 
 template <typename C> struct AlphaString
 {
-	const shared_ptr<Alphabet<C>> alphabet;
+	const shared_ptr<AlphabetLike<C>> alphabet;
 
 	/*
 	 * Invariant: all chars string[i] are in the alphabet S
@@ -25,10 +25,29 @@ template <typename C> struct AlphaString
 	size_t size() const;
 
 	static AlphaString<LetterChar>
-	parseString(std::shared_ptr<Alphabet<LetterChar>>, std::string const&);
+	parseString(shared_ptr<Alphabet<LetterChar>>, std::string const&);
+
+	static AlphaString<C> parseString(shared_ptr<AlphabetLike<C>> alphabet,
+	                                  std::string const& str)
+	{
+		std::vector<const C*> new_string;
+
+		for (C const& c : str) {
+			auto in_alphabet = alphabet->findChar(c);
+
+			if (in_alphabet == nullptr) {
+				throw std::invalid_argument(
+				    "Attempted to parse a character not in alphabet.");
+			}
+
+			new_string.push_back(in_alphabet);
+		}
+
+		return {alphabet, std::move(new_string)};
+	}
 
 private:
-	AlphaString(shared_ptr<Alphabet<C>> alphabet,
+	AlphaString(shared_ptr<AlphabetLike<C>> alphabet,
 	            const std::vector<const C*>&& string)
 	    : alphabet(alphabet), string(std::move(string))
 	{
