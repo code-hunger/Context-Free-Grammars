baff4ba3f27c741e93ac4eb4c77c6e3cf5ca014b
Code Hunger <beinghavingbreackfast@gmail.com>
2019-11-20 23:38:56 +0200

    Define an alphabet touple to hold the N and T alphabets.
    It's AlphabetLike.

--- Alphabet.h
+++ Alphabet.h
@@ -131,9 +131,45 @@ public:
 
 	~Alphabet()
 	{
-		for (const C* c : chars) {
-			delete c;
-		}
+		for_each([](const C* c) { delete c; });
+	}
+};
+
+template <typename C>
+bool pairwiseDistinct(Alphabet<C> const& A, Alphabet<C> const& B)
+{
+	return all_of(A, [&B](const C* c) { return !B.findChar(*c); });
+}
+
+template <typename C> struct AlphabetTouple : public AlphabetLike<C>
+{
+	const std::shared_ptr<Alphabet<C>> N, T;
+
+	const C* findChar(C const& c) const override
+	{
+		// c is guaranteed to be at most in one of N and T.
+		if (auto a = N->findChar(c)) return a;
+		return T->findChar(c);
+	};
+
+	bool subsetOf(AlphabetLike<C> const& other) const override
+	{
+		auto otherHasChar = [&other](const C* c) { return other.findChar(*c); };
+		return all_of(*N, otherHasChar) && all_of(*T, otherHasChar);
+	};
+
+	void print(std::ostream& out) const override
+	{
+		auto printChar = [&out](const C* c) { c->print(out); };
+		N->for_each(printChar);
+		T->for_each(printChar);
+	}
+
+	AlphabetTouple(decltype(N) N, decltype(T) T) : N(N), T(T)
+	{
+		if (!pairwiseDistinct(*N, *T))
+			throw std::invalid_argument(
+			    "The two alphabets must be pairwise distinct.");
 	}
 };
 
