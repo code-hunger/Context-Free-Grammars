57d574752fb0b6b1b90c191c2b65e11d3cc04764
Code Hunger <beinghavingbreackfast@gmail.com>
2019-11-26 02:04:46 +0200

    Take the StackCommand and its derivatives to their own header file.
    No need for all of this in Stack.h.
    Let's keep at least that one simple. It's a simple stack, in the end.
    Not a bunch of ugly OOP stuff.
    Time to go to sleep, finally.

---
+++ StackCommands.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <ostream>
+
+#include "Stack.h"
+
+namespace context_free {
+
+template <typename C> struct StackCommand
+{
+	virtual void print(std::ostream&) const = 0;
+	virtual void execute(Stack<C>& stack) = 0;
+
+	virtual void undo(Stack<C>& stack) = 0;
+
+	virtual ~StackCommand() {}
+};
+
+template <typename C> struct Pop : StackCommand<C>
+{
+	void print(std::ostream& out) const override { out << "Pop"; }
+
+	void execute(Stack<C>& stack) override
+	{
+		if (stack.empty())
+			throw std::runtime_error("Cannot call Pop() on an empty stack");
+
+		valuePopped = stack.top();
+		stack.pop();
+	}
+
+	void undo(Stack<C>& stack) override
+	{
+		if (!valuePopped.has_value())
+			throw std::runtime_error("This Pop command has not been executed "
+			                         "or has been already undone.");
+
+		stack.push(*valuePopped);
+		valuePopped = std::nullopt;
+	}
+
+	~Pop() {}
+
+private:
+	std::optional<C> valuePopped;
+};
+
+template <typename C> struct Sleep : StackCommand<C>
+{
+	void print(std::ostream& out) const override { out << "Sleep"; }
+
+	void execute(Stack<C>&) override {}
+
+	void undo(Stack<C>&) override {}
+
+	~Sleep() {}
+};
+
+template <typename C> struct Push : StackCommand<C>
+{
+	const C what;
+	void print(std::ostream& out) const override
+	{
+		out << "Push<" << what << ">";
+	}
+
+	void execute(Stack<C>& stack) override { stack.push(what); }
+
+	void undo(Stack<C>& stack) override
+	{
+		if (!stack.top().has_value())
+			throw std::runtime_error(
+			    "Can't undo a 'Push' operation on an empty stack.");
+
+		if (*stack.top() != what)
+			throw std::runtime_error(
+			    "The stack top is not how this Push command would have left "
+			    "it. Refusing to undo.");
+
+		stack.pop();
+	}
+
+	~Push() {}
+};
+
+template <typename C> struct Replace : StackCommand<C>
+{
+	const C with;
+	void print(std::ostream& out) const override
+	{
+		out << "Replace<" << with << ">";
+	}
+
+	void execute(Stack<C>& stack) override
+	{
+		if (stack.empty())
+			throw std::runtime_error(
+			    "Tried to replace the top of an empty stack!");
+
+		const C* inAlphabet = stack.alphabet->findChar(with);
+
+		if (!inAlphabet)
+			throw std::runtime_error("Got an error while executing "
+			                         "Replace() command on the stack. "
+			                         "Stack left unchanged.");
+
+		valueReplaced = stack.top();
+
+		stack.pop();
+		stack.push(with);
+	}
+
+	void undo(Stack<C>& stack)
+	{
+		if (!valueReplaced.has_value())
+			throw std::runtime_error("This Replace command has not been "
+			                         "executed or has been already undone.");
+
+		if (!stack.top().has_value())
+			throw std::runtime_error(
+			    "Can't undo a 'Replace' operation on an empty stack.");
+
+		if (*stack.top() != with)
+			throw std::runtime_error(
+			    "The stack top is not how this Replace command would have left "
+			    "it. Refusing to undo.");
+
+		stack.pop();
+		stack.push(valueReplaced);
+		valueReplaced = std::nullopt;
+	}
+
+	~Replace() {}
+
+private:
+	std::optional<C> valueReplaced;
+};
+
+} // namespace context_free
