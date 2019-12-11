433b35eaba98c2c08706ade8f0a2afe0b79c04fa
Code Hunger <beinghavingbreackfast@gmail.com>
2019-12-12 00:13:01 +0200

    Implement a simple advance() algorithm on ReadState

--- ReadAlgorithm.h
+++ ReadAlgorithm.h
@@ -3,7 +3,7 @@
 #include "AlphaString.h"
 #include "MeatBall.h"
 #include "Stack.h"
-#include <forward_list>
+#include <list>
 
 namespace context_free {
 
@@ -15,16 +15,46 @@ template <typename CTerminal, typename CStack> class ReadState
 	using StateHead =
 	    std::tuple<MeatBall<CTerminal, CStack>*, Stack<CStack>, WordPtr>;
 
-	std::forward_list<StateHead> heads;
+	std::list<StateHead> heads;
 
 	using HeadsIt = typename decltype(heads)::iterator;
+
+	bool advance(HeadsIt head)
+	{
+		auto& [meatBall, stack, nextChar] = *head;
+
+		if (nextChar == word.string.end()) { // No more chars to read.
+			// Hope we found it
+			return stack.empty();
+		}
+
+		auto const& transitions = meatBall->next(stack, *nextChar);
+
+		for (auto const& [cmd, targetMeatBall] : transitions) {
+			auto newStack{stack};
+			cmd->execute(newStack);
+
+			heads.push_front(
+			    std::make_tuple(targetMeatBall, newStack, nextChar + 1));
+		}
+
+		return false;
+	}
 
 public:
 	const Word word;
 
-	bool advance(HeadsIt head)
+	std::optional<StateHead> advance()
 	{
-		throw std::runtime_error("To be implemented.");
+		for (auto it = heads.begin(); it != heads.end(); ++it) {
+			if (advance(*it)) {
+				return *it;
+			}
+
+			heads.erase(it);
+		}
+
+		return std::nullopt;
 	}
 };
 
